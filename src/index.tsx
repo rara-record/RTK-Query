aec7385fa56ba06adb20ad2fe8234008e6aad264
bora <a52447879@gmail.com>
2022-05-02 20:38:26 +0900

    [feat] 필요한컴포넌트 생성 및 불필요한 파일 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
